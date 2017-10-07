04740b6d4c76d270492dd00c11d3946f39b6151c
mtli <martinli.work@gmail.com>
2017-10-06 23:57:29 -0400

    Added to pypi

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Researcher',
-        'Topic :: Research :: Visualization Tools',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
